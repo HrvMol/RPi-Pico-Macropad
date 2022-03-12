5baf7aa74e385729001af7cae1416aafe0c4d97d
Harvin Molyneux <84520213+Joxnt@users.noreply.github.com>
2022-03-12 21:16:57 +0000

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,13 +1,15 @@
 import storage, usb_cdc
 import board, digitalio
 
-btn = digitalio.DigitalInOut(board.GP3)
+#button used to show the drive
+btn = digitalio.DigitalInOut(board.GP9)
 btn.direction = digitalio.Direction.INPUT
 btn.pull = digitalio.Pull.DOWN
 
-if not btn.value:
-    storage.disable_usb_drive()
-    usb_cdc.disable()
-else:
+#shows or hides storage and cdc depending on button state
+if btn.value:
     storage.enable_usb_drive()
     usb_cdc.enable()
+else:
+    storage.disable_usb_drive()
+    usb_cdc.disable()
